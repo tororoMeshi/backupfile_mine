3d7554e0d944312ce2d6ffa6d97cc2f2d422c262
tororoMeshi <8noki8@gmail.com>
2023-02-19 14:11:26 +0000

    back-up Sun Feb 19 02:11:26 PM UTC 2023

--- conf/start.sh
+++ conf/start.sh
@@ -10,7 +10,7 @@
 # -Xmx<size> : specifies the maximum size of the memory allocation pool in bytes
 # -jar: run the program encapsulated in a JAR file
 
-cd /home/ubuntu/paper_minecraft/
+cd ~/backupfile_mine/executable
 
 #java -Xmx1024M -Xms1024M -jar 1.18-server.jar nogui
 #java -Xmx6144M -Xms1024M -jar 1.18.1-server.jar nogui
@@ -21,6 +21,6 @@ cd /home/ubuntu/paper_minecraft/
 # I recommend using at least 6-10GB, No matter how few players! If you can’t afford 10GB of memory, give as much as you can, but ensure you leave the operating system some memory too. G1GC operates better with more memory.
 #java -Xmx6144M -Xms6144M -jar paper-1.18.2-312.jar nogui
 #java -Xmx6144M -Xms6144M -jar paper-1.19.2-133.jar nogui
-java -Xmx6144M -Xms6144M -jar paper-1.19.3-368.jar nogui
+java -Xmx6144M -Xms6144M -jar minecraft_server.1.19.3.jar nogui
 
 
